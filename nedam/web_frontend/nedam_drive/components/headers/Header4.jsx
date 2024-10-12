8e0b64fe3cddca8ba530d0da90d1058c676fe06f
Fares <fares.djelili95@gmail.com>
2024-10-12 12:23:12 +0200

    modify header4.jsx file

--- nedam/web_frontend/nedam_drive/components/headers/Header4.jsx
+++ nedam/web_frontend/nedam_drive/components/headers/Header4.jsx
@@ -5,6 +5,8 @@ import Nav from "./components/Nav";
 import Image from "next/image";
 import Link from "next/link";
 import Language from "./components/Language";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaSearch, FaPlusCircle, FaUserCircle } from "react-icons/fa";
 
 export default function Header4() {
   const [scrolled, setScrolled] = useState(false);
@@ -34,6 +36,7 @@ export default function Header4() {
       <div className="container-fluid box-header-home4">
         <div className="main-header">
           <div className="header-left">
+            {/* Logo : */}
             <div className="header-logo">
               <Link className="d-flex" href="/">
                 <Image
@@ -44,6 +47,8 @@ export default function Header4() {
                 />
               </Link>
             </div>
+
+            {/* header-nav: */}
             <div className="header-nav">
               <nav className="nav-main-menu d-none d-xl-block">
                 <ul className="main-menu">
@@ -55,7 +60,9 @@ export default function Header4() {
                 <span className="burger-icon-bottom"></span>
               </div>
             </div>
-            <div className="header-right">
+
+            {/* header-right original : */}
+            {/* <div className="header-right">
               <div className="d-none d-xxl-inline-block align-middle mr-10">
                 <a
                   className="text-14-medium call-phone color-white hover-up"
@@ -76,6 +83,70 @@ export default function Header4() {
                 <Link className="btn btn-white hover-up" href="/register">
                   Sign Up
                 </Link>
+              </div>
+            </div> */}
+
+            {/* header-right modified : */}
+            <div className="header-right d-flex align-items-center justify-content-end">
+              <div className="d-inline-block mr-4">
+                <Link
+                  href="/search_traject"
+                  className="d-flex align-items-center custom-link"
+                >
+                  <FaSearch className="custom-icon search-icon" />
+                  <span className="custom-text">Rechercher un trajet</span>
+                </Link>
+              </div>
+
+              <div
+                className="d-inline-block"
+                style={{ marginLeft: "20px", marginRight: "40px" }}
+              >
+                <Link
+                  href="/publish_traject"
+                  className="d-flex align-items-center custom-link"
+                >
+                  <FaPlusCircle className="custom-icon publish-icon" />
+                  <span className="custom-text">Publier un trajet</span>
+                </Link>
+              </div>
+
+              <div className="d-none d-xxl-inline-block box-dropdown-cart align-middle mr-10">
+                <Language />
+              </div>
+
+              <div className="dropdown d-inline-block">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="userMenuButton"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FaUserCircle style={{ fontSize: "3em" }} />
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="userMenuButton"
+                  style={{ minWidth: "200px" }}
+                >
+                  <li>
+                    <Link
+                      className="dropdown-item d-flex align-items-center"
+                      href="/login"
+                    >
+                      <span className="ml-2">Connexion</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item d-flex align-items-center"
+                      href="/register"
+                    >
+                      <span className="ml-2">Inscription</span>
+                    </Link>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
