3c227ec4f53cafc2a5ce7f40a609753a934ab685
mohamed-linux <mohamedkarassane@gmail.com>
2024-10-14 11:00:40 +0200

    added services of home 10 into homepage

--- nedam/web_frontend/nedam_drive/app/page.jsx
+++ nedam/web_frontend/nedam_drive/app/page.jsx
@@ -7,7 +7,7 @@ import DownloadApp from "@/components/homes/home-4/DownloadApp";
 import Features from "@/components/homes/common/features/Features";
 import Feet from "@/components/homes/home-4/Feet";
 import Hero from "@/components/homes/home-4/Hero";
-import Service from "@/components/homes/home-4/Service";
+import Services from "@/components/homes/home-10/Services";
 import Steps from "@/components/homes/home-4/Steps";
 import MobailHeader1 from "@/components/headers/MobailHeader1";
 export const metadata = {
@@ -22,9 +22,9 @@ export default function page() {
       <Header4 /> <MobailHeader1 />
       <main className="main">
         <Hero />
+        <Services />
         <DownloadApp />
         <Steps />
-        <Service />
         <Feet />
         <Features />
         <div className="border-bottom mt-30"></div>
