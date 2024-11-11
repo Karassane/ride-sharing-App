66a42423d29b0d859caa46df195fcf5330249b01
Fares <fares.djelili95@gmail.com>
2024-11-11 14:34:41 +0100

    delete templates folder

--- nedam/templates/luxdrive_template/luxride/components/common/DatePicker.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React, { useState } from "react";
-import DatePicker from "react-multi-date-picker";
-
-export default function DatePickerComponent() {
-  const [value, setValue] = useState(new Date());
-
-  return <DatePicker format="MMMM DD YYYY" value={value} onChange={setValue} />;
-}
