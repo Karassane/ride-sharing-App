fcd20249e884e76f014294489136827196ef92a7
Fares <fares.djelili95@gmail.com>
2024-12-01 13:01:31 +0100

    Add Filters component to home-10 page

--- nedam/web_frontend/nedam_drive/components/homes/home-10/Hero.jsx
+++ nedam/web_frontend/nedam_drive/components/homes/home-10/Hero.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import TripCard from "@/components/TripCard/TripCard";
+import Filters from "@/components/Filters/Filters"; // Importez le composant Filters
 import { useRouter } from "next/navigation"; // Importer useRouter
 
 export default function Hero() {
@@ -12,22 +13,25 @@ export default function Hero() {
   };
 
   return (
-    <div className="centered-container">
-      <TripCard
-        date="Demain"
-        fromCity="Paris"
-        toCity="Algiers"
-        departureTime="00:30"
-        arrivalTime="02:40"
-        duration="2h10"
-        price="9,99"
-        driver={{
-          name: "Fares",
-          avatar: "/assets/imgs/page/driver/driver_1.webp",
-          rating: "4.8",
-        }}
-        onClick={handleTripCardClick} // Passez la fonction de clic
-      />
+    <div className="hero-container">
+      <div className="centered-container">
+        <Filters /> {/* Ajoutez les filtres ici */}
+        <TripCard
+          date="Demain"
+          fromCity="Paris"
+          toCity="Algiers"
+          departureTime="00:30"
+          arrivalTime="02:40"
+          duration="2h10"
+          price="9,99"
+          driver={{
+            name: "Fares",
+            avatar: "/assets/imgs/page/driver/driver_1.webp",
+            rating: "4.8",
+          }}
+          onClick={handleTripCardClick} // Passez la fonction de clic
+        />
+      </div>
     </div>
   );
 }
