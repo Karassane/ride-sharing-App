cd0222b6b4a29d65d9ead56d5eb82b470fc6f572
Fares <fares.djelili95@gmail.com>
2024-12-07 18:20:18 +0100

    merge commit

---
+++ nedam/web_frontend/nedam_drive/data/menu_BACKUP_74311.js
@@ -0,0 +1,36 @@
+export const menuItems = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+    subMenu: [
+      { id: 11, title: "Homepage 1", link: "/" },
+      { id: 12, title: "Homepage 2", link: "/home-2" },
+      { id: 13, title: "Homepage 3", link: "/home-3" },
+      { id: 14, title: "Homepage 4", link: "/home-4" },
+      { id: 15, title: "Homepage 5", link: "/home-5" },
+      { id: 16, title: "Homepage 6", link: "/home-6" },
+      { id: 17, title: "Homepage 7", link: "/home-7" },
+      { id: 18, title: "Homepage 8", link: "/home-8" },
+      { id: 19, title: "Homepage 9", link: "/home-9" },
+      { id: 20, title: "Homepage 10", link: "/home-10" },
+      { id: 21, title: "nedam drive 2nd", link: "/nedam-drive-2nd" },
+<<<<<<< HEAD
+      { id: 22, title: "Carpool Reservation", link: "/carpool-reservation" },
+      { id: 23, title: "nedam Drive Mainpage", link: "/daily-reservation-main" },
+=======
+>>>>>>> fares_ideapad_11112024
+    ],
+  },
+  // {
+  //   id: 4,
+  //   title: "Driver",
+  //   link: "#",
+  //   subMenu: [
+  //     { id: 41, title: "Service Listing", link: "/service-grid" },
+  //     { id: 42, title: "Service Listing 2", link: "/service-grid-2" },
+  //     { id: 43, title: "Service Listing 3", link: "/service-grid-3" },
+  //     { id: 44, title: "Service Single", link: "/service-single/2" },
+  //   ],
+  // },
+];
